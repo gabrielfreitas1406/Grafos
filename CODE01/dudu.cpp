3e334cd5a1a1e7fbce5b3afbf2c2fb79815efc2d
Gabriel Freitas <112510986+gabrielfreitas1406@users.noreply.github.com>
2024-02-07 18:17:19 -0400

    Update dudu.cpp

--- CODE01/dudu.cpp
+++ CODE01/dudu.cpp
@@ -1,57 +1,60 @@
-#include <bits/stdc++.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
-const int BRANCO = 0;
-const int CINZA = 0;
-const int PRETO = 0;
 
-vector<int> LA[10010];
-int vis[10010];
-int n, m, u, v;
-int hasCycle = 0;
+int t,n,m,a,b;
+vector<int> g[10010];
 
+// 0 - Branco, 1 - Cinza, 2 - Preto
+int color[10010];
 
-void dfs(int u){
-    vis[u] = CINZA;
-    for(int v: LA[u]){
-        if (vis[v] == BRANCO){
-            dfs(v);
-        }
-        else if (vis[v] == CINZA){
-            hasCycle = 1;
+int checkCycle(int u){
+    color[u] = 1; // Preto
+
+    int cycle = false;
+    for(int v: g[u]){
+        if(color[v] == 1) return true;
+
+        if(color[v] == 0){
+            if(checkCycle(v)) cycle = true;
         }
     }
+
+    color[u] = 2; // Cinza
+
+    return cycle;
 }
 
+
 int main(){
-    int T;
-    cin >> T;
-    while(T--){
+    cin >> t;
+
+    while(t--){
         cin >> n >> m;
-        hasCycle = 0;
-
-        for(int i=1; i<=n; i++){
-            vis[i] = BRANCO;
-            LA[i].clear();
+        
+        //Reseta o Grafo.
+        for(int i = 1;i<=n;i++){
+            g[i].clear();
+            color[i] = 0;
         }
 
+        //Le e constrói o grafo.
         for(int i=0;i<m;i++){
-            cin >> u >> v;
-            LA[u].push_back(v);
+            cin >> a >> b;
+            g[b].push_back(a);
         }
 
-        for(int i=1; i<=n; i++){
-            if (vis[i] == BRANCO){
-                dfs(i);
-                if (hasCycle) break;
+        // Checa se existe ciclo.
+        int hasCycle = 0;
+        for(int i=1;i<=n;i++){
+            if(color[i] == 0 && checkCycle(i)){
+                hasCycle = 1;
             }
         }
 
-        if (hasCycle) cout << "SIM\n";
-        else cout << "NÃO\n";
+        if(hasCycle) cout << "SIM\n";
+        else cout << "NAO\n";
     }
-
 }
